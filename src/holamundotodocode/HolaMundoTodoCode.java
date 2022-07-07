fe5b5d82d5ea3d50759aecf1df179f457ac237ea
rickyalfred2022 <coronel.ricardo.55@gmail.com>
2022-07-07 16:18:43 -0300

    subiendo_algo_de_java

---
+++ src/holamundotodocode/HolaMundoTodoCode.java
@@ -0,0 +1,14 @@
+
+package holamundotodocode;
+
+public class HolaMundoTodoCode {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola Mundo!!!");
+        System.out.println("Suscribite a TodoCode y dale like");
+        
+    }
+
+}
